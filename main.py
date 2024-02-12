e565cc86aad8c0bf7ae3a61da2cbe4ace78d08ef
lifix1 <daniil.potebnia@gmail.com>
2024-02-12 19:01:02 +0300

    fifth task

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -26,6 +26,21 @@ def prom():
 def mars():
     return render_template('mars.html')
 
+
+@app.route('/astronaut_selection', methods=['GET', 'POST'])
+def astronaut_selection():
+    if request.method == 'GET':
+        return render_template('form.html')
+    elif request.method == 'POST':
+        print(request.form['surname'])
+        print(request.form['name'])
+        print(request.form['email'])
+        print(request.form['educ'])
+        print(request.form['prof'])
+        print(request.form['gender'])
+        print(request.form['motiv'])
+        print('Да') if request.form['stay_on_mars'] == 'on' else print('Нет')
+        return render_template('form.html')
 
 
 if __name__ == '__main__':
